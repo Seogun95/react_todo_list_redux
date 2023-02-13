81e6870580cccad691ef00a2bb097d50a5250418
Seogun95 <ksj083895@gmail.com>
2023-02-14 02:09:01 +0900

    feat: detail 페이지 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -8,14 +8,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import TodoDetail from '../pages/TodoDetail';
+import Layout from './Layout';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<TodoDetail />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<TodoDetail />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
