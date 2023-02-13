288f74ac846dc775d3482fde8bbcb2b75f6a52bf
Seogun95 <ksj083895@gmail.com>
2023-02-13 19:11:52 +0900

    feat: Home.jsx와 router 연결

---
+++ src/shared/Router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// 1. react-router-dom을 사용하기 위해서 아래 API들을 import 한다.
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// 2. Router 라는 함수를 만들고 아래와 같이 작성한다.
+//BrowserRouter를 Router로 감싸는 이유는,
+//SPA의 장점인 브라우저가 깜빡이지 않고 다른 페이지로 이동할 수 있게 만들어준다!
+
+import Home from '../pages/Home';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
